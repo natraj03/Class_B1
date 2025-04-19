44327dc12763931b64ab7c60646ee676e8fa8eb4
az440061 <connectnataraj03@gmail.com>
2025-04-19 16:13:38 +0530

    Initial Commit

---
+++ Lesson1.py
@@ -0,0 +1,21 @@
+
+
+inp1 = 5.4
+inp2 = -2
+
+# print(inp1 + inp2) # add
+# print(inp1 * inp2) # multiply
+# print(inp1 / inp2) # divide
+
+def multFun(var1, var2):
+    return var1 * var2
+
+def addfun(xyz, uvw):
+    return xyz + uvw
+
+def div(div1, div2):
+    return div1 / div2
+
+
+
+
---
+++ test_lesson1.py
@@ -0,0 +1,10 @@
+import pytest
+
+from Lesson1 import multFun, addfun, div
+
+def test_mult_positive():
+    assert multFun(5,5) == 25
+
+
+def test_div():
+    assert div(4,2) == 2
